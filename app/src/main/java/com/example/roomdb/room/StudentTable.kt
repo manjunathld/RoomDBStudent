1fd64f5b41f21ec9eaac2f4d1a92f5cbaad73dcf
Manjunath LD <ldmanju007@gmail.com>
2022-03-09 00:45:39 +0530

    Commit_003: For the Room components following update is done 1. Entity: Student Table created 2. Dao: GetAll, Find by name, Insert, Update and Delete Queries are mapped with functions 3. Database: Singleton RoomDB object is created with the help of Room.databaseBuilder() method.

--- app/src/main/java/com/example/roomdb/room/StudentTable.kt
+++ app/src/main/java/com/example/roomdb/room/StudentTable.kt
@@ -1,7 +1,22 @@
 package com.example.roomdb.room
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
 
 @Entity(tableName = "student_table")
-class StudentTable {
-}
+data class StudentTable
+    (
+        @NotNull
+        @PrimaryKey(autoGenerate = true)
+        var id: Int,
+
+        @NotNull
+        @ColumnInfo(name = "name")
+        var name: String,
+
+        @NotNull
+        @ColumnInfo(name = "result")
+        var result: Double
+    ) {}
--- app/src/main/java/com/example/roomdb/room/StudentTableDAO.kt
+++ app/src/main/java/com/example/roomdb/room/StudentTableDAO.kt
@@ -1,7 +1,21 @@
 package com.example.roomdb.room
 
-import androidx.room.Dao
+import androidx.room.*
 
 @Dao
 interface StudentTableDAO {
+    @Query("SELECT * FROM student_table")
+    fun getAllStudentTable() : List<StudentTable>
+
+    @Query("SELECT * FROM student_table WHERE name IN (:name)")
+    fun loadAllStudentTableByName(name: String)
+
+    @Insert
+    fun insertStudentTable(vararg studentTable: StudentTable)
+
+    @Update
+    fun updateStudentTable(vararg studentTable: StudentTable)
+
+    @Delete
+    fun deleteStudentTable(vararg studentTable: StudentTable)
 }
